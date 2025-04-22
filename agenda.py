493ec4734636ca434b7f64d672139551bdcf65de
alessandroibes <alessandro.ibes@gmail.com>
2025-04-22 08:22:45 -0300

    Adicionado menu com a lista de opções do que é possível fazer no app, permitindo que o usuário digite uma escolha para iniciar a aplicação

---
+++ agenda.py
@@ -0,0 +1,36 @@
+agenda = []
+while True:
+    print("\nMenu da Agenda de Contatos:")
+    print("1. Adicionar um novo contato")
+    print("2. Visualizar a lista de contatos cadastrados")
+    print("3. Editar um contato")
+    print("4. Marcar/desmarcar um contato como favorito")
+    print("5. Visualizar a lista de contatos favoritos")
+    print("6. Apagar um contato")
+    print("7. Sair")
+
+    escolha = input("Digite a sua escolha: ")
+
+    match escolha:
+        case "1":
+            print("Funcionalidade ainda não implementada.")
+        
+        case "2":
+            print("Funcionalidade ainda não implementada.")
+
+        case "3":
+            print("Funcionalidade ainda não implementada.")
+
+        case "4":
+            print("Funcionalidade ainda não implementada.")
+
+        case "5":
+            print("Funcionalidade ainda não implementada.")
+
+        case "6":
+            print("Funcionalidade ainda não implementada.")
+
+        case "7":
+            break
+
+print("Programa finalizado")
